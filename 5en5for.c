b04369c99bb3af3e73f71a9b3ce7656086144a3c
mariagsn <168490968+mariagsn@users.noreply.github.com>
2024-05-03 17:52:09 -0600

    Add files via upload

---
+++ 5en5for.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+int nm, i;
+printf ("cuantos terminos quieres que se muestren ");
+scanf ("%d", &nm);
+ for (i=1; i<=nm; i++)
+  {
+  printf(" %d", nm + 5);
+  }
+  return 0;
+}
