86afd839ff118eeb83402baeb88d9de4652ff5bf
mariagsn <168490968+mariagsn@users.noreply.github.com>
2024-05-03 19:22:46 -0600

    Add files via upload

---
+++ usuario.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main () 
+{ 
+	char nom[15];
+	int con;
+	printf ("ingresa tu nombre");
+	scanf ("%c", &nom);
+	if (nom = root){
+		printf("tu nombre es correcto");
+	}
+	else if(nom=x){
+		printf("tu nombres incorrecto");
+    }
+	printf("ingresa la contraseña");
+	scanf ("%c", &con);
+	if (con==1234){
+		printf("tu contraseña es correcta");
+	}
+	else if(con==2){
+		printf("tu contraseña es incorrecta");
+    }
+    return 0;
+}
