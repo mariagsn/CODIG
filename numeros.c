d0dcc4e03d49ea1a28226f86711c64b21e7c6a86
mariagsn <168490968+mariagsn@users.noreply.github.com>
2024-05-03 19:14:56 -0600

    Add files via upload

--- numeros.c
+++ numeros.c
@@ -2,15 +2,20 @@
 
 int main () 
 {
-  int num;  
+  int num, sm=0, cont=0;  
   do
   {
    printf ("hola, ingresa un numero ");
    scanf ("%d",&num);
-   while (num!=0);
-    {
-   printf (" tu numero ingresado fue\n %d",num );
-   }
+   if (num!=0){
+   printf (" tu numero ingresado fue%d\n",num );
+   sm+=num;
+   cont++;
   }
+  }while(num!=0);
+  
+  printf ("finalizando por introducir el numero 0.\n");
+  printf ("la cantidad de numeros introducidos fue de :%d\n", cont);
+  printf ("la suma de todos los numeros fue de:%d\n", sm );
 return 0;
 }
