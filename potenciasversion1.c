2a7f325aec1f9a13ff2aa544042a127c8118788b
mariagsn <168490968+mariagsn@users.noreply.github.com>
2024-05-06 17:45:50 -0600

    Add files via upload

---
+++ potenciasversion1.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+double potencia (double bs, double ex) {
+
+return pow(bs,ex);
+
+}
+
+int main ()
+
+{
+double bs,ex, resul;
+printf ("ingresa un numero");
+scanf("%d", &b);
+printf ("ingresa a la potencia");
+scanf ("%d", &ex);
+resul = pow(bs,ex)
+printf ("%.2f elevado por %.2f es de %.2f\n", bss,ex, resul);
+return 0;
+
+}
