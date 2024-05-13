7c461c367a55a7f3a7ec2dcfc9d6b59f9894c679
mariagsn <168490968+mariagsn@users.noreply.github.com>
2024-05-13 11:44:12 -0600

    Add files via upload

---
+++ contversion3.c
@@ -0,0 +1,45 @@
+#include <stdio.h> //VERSION 3 EXAMEN PRACTICO 13-05-2024
+#include <string.h>
+
+int main() {
+    char nom[20];
+    char cont[20];
+    int pro, cant;
+    int intentos = 3;
+    int res,imp, importe;
+
+    do {
+        printf("Introduce tu nombre: ");
+        scanf("%s", nom);
+
+        printf("Introduce tu contraseña: ");
+        scanf("%s", cont);
+
+        if (strcmp(nom, "maria") == 0 && strcmp(cont, "2b38") == 0) {
+            printf("Bienvenido al sistema.\n"); 
+            break;
+        } else {
+            intentos--;
+            if (intentos > 0) {
+                printf("Nombre de usuario o contrasea incorrectos. Te quedan %d intentos.\n", intentos);
+            } else {
+                printf("Se ha superado el número de intentos permitido.\n");
+                break;
+            }
+        }
+    } while (cant != 0);
+    printf ("para terminar introduce la cantidad de 0 \n\n");
+      printf("Muy bien, Ingresa la cantidad de tus articulos vendidos  ");
+        scanf  ("%d", &pro);
+        printf ("de que precio fueron? $");
+        scanf ("%d", &cant);
+        if (pro < 0||cant < 0 ){
+            printf ("lo siento, tus cantidades son negativas, Ingresa otra cantidad .\n\n");
+           } else {
+                float imp=pro*cant;
+                importe +=imp;
+            }
+       printf ("tu total de los productos es de %d pesos ", importe); 
+
+    return 0;
+}
